1e0aaf5d5a898878d0ab6c4416c3aad4fa1afba2
Sushil Kumar <johnklutz.sk@gmail.com>
2022-08-10 11:29:50 +0100

    moved email config to appsettings and user secrets

--- EMS.Core.Application/Domain/Users/Services/MailService.cs
+++ EMS.Core.Application/Domain/Users/Services/MailService.cs
@@ -1,18 +1,31 @@
 ﻿using MimeKit;
 using MailKit.Net.Smtp;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 
 namespace EMS.Core.Application.Domain.Users.Services
 {
     public class MailService : IMailService
     {
+        public IConfiguration Configuration { get; private set; }
+
+        public MailService(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         public async Task SendEmailAsync(string toEmail, string subject, string content)
         {
-            // TODO Some values here should be provided via config.
+            string username = Configuration["EmailConfig:Username"];
+            string password = Configuration["EmailConfig:Password"];
+            string smtpHost = Configuration["EmailConfig:SMTPHost"];
+            int port = int.Parse(Configuration["EmailConfig:Port"]);
+            string senderAddress = Configuration["EmailConfig:SenderAddress"];
+            string senderName = Configuration["EmailConfig:SenderName"];
 
             MimeMessage message = new MimeMessage();
 
-            MailboxAddress from = new MailboxAddress("Hello World", "no-reply@ems.com");
+            MailboxAddress from = new MailboxAddress(senderName, senderAddress);
             message.From.Add(from);
 
             MailboxAddress to = new MailboxAddress(toEmail, toEmail);
@@ -22,9 +35,11 @@ namespace EMS.Core.Application.Domain.Users.Services
             BodyBuilder bodyBuilder = new BodyBuilder();
             bodyBuilder.HtmlBody = content;
             message.Body = bodyBuilder.ToMessageBody();
+
+
             SmtpClient client = new SmtpClient();
-            client.Connect("smtp-relay.sendinblue.com", 587, false);
-            client.Authenticate("johnklutz.sk@gmail.com", "OC7b36sy4IzPrHKF");
+            client.Connect(smtpHost, port, false);
+            client.Authenticate(username, password);
             await client.SendAsync(message);
             client.Disconnect(true);
             client.Dispose();
