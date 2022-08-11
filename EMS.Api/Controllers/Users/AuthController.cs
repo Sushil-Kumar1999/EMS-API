7eb16fedaa7829cf9e9ee42f184de77877fe2472
Sushil Kumar <johnklutz.sk@gmail.com>
2022-08-11 13:39:19 +0100

    moved register user action to users controller

--- EMS.Api/Controllers/Users/AuthController.cs
+++ EMS.Api/Controllers/Users/AuthController.cs
@@ -5,9 +5,6 @@ using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
-using System.Collections.Generic;
-using EMS.Core.Application.Domain.Enums;
-using Microsoft.AspNetCore.Authorization;
 
 namespace EMS.Api.Controllers.Users
 {
@@ -21,23 +18,6 @@ namespace EMS.Api.Controllers.Users
         public AuthController(IMediator mediator)
         {
             _mediator = mediator;
-        }
-
-        [HttpPost("register")]
-        //[Authorize(Roles = nameof(UserRoles.Admin))]
-        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(string))]
-        //[ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(ValidationErrorsResponse))]
-        public async Task<IActionResult> RegisterAsync([FromBody] UserRegistrationRequestDataContract request)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest("Invalid payload");
-            }
-
-            var command = new RegisterUserCommand(request.FirstName, request.LastName, request.Email, request.Role);
-            UserRegistrationResponseDto response = await _mediator.Send(command);
-
-            return Ok(response);
         }
 
         [HttpPost("login")]
--- EMS.Api/Controllers/Users/UsersController.cs
+++ EMS.Api/Controllers/Users/UsersController.cs
@@ -1,14 +1,18 @@
 ﻿using EMS.Core.Application.Domain.Enums;
+using EMS.Core.Application.Domain.Users.Commands;
 using EMS.Core.Application.Domain.Users.Queries;
+using EMS.Core.DataTransfer.Users.DataContracts;
+using EMS.Core.DataTransfer.Users.DTOs;
 using MediatR;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 
 namespace EMS.Api.Controllers.Users
 {
-    [Route("[controller]")]
+    [Route("users")]
     [ApiController]
     [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     public class UsersController : ControllerBase
@@ -18,6 +22,23 @@ namespace EMS.Api.Controllers.Users
         public UsersController(IMediator mediator)
         {
             _mediator = mediator;
+        }
+
+        [HttpPost("register")]
+        //[Authorize(Roles = nameof(UserRoles.Admin))]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(string))]
+        //[ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(ValidationErrorsResponse))]
+        public async Task<IActionResult> RegisterAsync([FromBody] UserRegistrationRequestDataContract request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("Invalid payload");
+            }
+
+            var command = new RegisterUserCommand(request.FirstName, request.LastName, request.Email, request.Role);
+            UserRegistrationResponseDto response = await _mediator.Send(command);
+
+            return Ok(response);
         }
 
         [HttpGet]
